e0a880ff9ddd9304b8a9627748083209dc5e1a50
sravanparsha <sravanparsha@gmail.com>
2025-12-18 12:08:15 +0530

    Create latest.py

---
+++ latest.py
@@ -0,0 +1,47 @@
+import gradio as gr
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.vectorstores import Chroma
+from langchain_community import embeddings
+from langchain_community.chat_models import ChatOllama
+from langchain_community.embeddings import OllamaEmbeddings
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_text_splitters import CharacterTextSplitter
+
+def process_input(pdf, question):
+    model_local = ChatOllama(model="llama3.2")
+
+    # Load the PDF document
+    docs = PyPDFLoader(pdf).load()
+
+    text_splitter = CharacterTextSplitter.from_tiktoken_encoder(chunk_size=7500, chunk_overlap=100)
+    doc_splits = text_splitter.split_documents(docs)
+
+    vectorstore = Chroma.from_documents(
+        documents=doc_splits,
+        collection_name="rag-chroma",
+        embedding=embeddings.ollama.OllamaEmbeddings(model='nomic-embed-text'),
+    )
+    retriever = vectorstore.as_retriever()
+
+    after_rag_template = """Answer the question based only on the following context:
+ {context}
+    Question: {question}
+    """
+    after_rag_prompt = ChatPromptTemplate.from_template(after_rag_template)
+    after_rag_chain = (
+        {"context": retriever, "question": RunnablePassthrough()}
+        | after_rag_prompt
+        | model_local
+        | StrOutputParser()
+    )
+    return after_rag_chain.invoke(question)
+
+# Define Gradio interface
+iface = gr.Interface(fn=process_input,
+                     inputs=[gr.File(label="Upload PDF"), gr.Textbox(label="Question")],
+                     outputs="text",
+                     title="Document Query with Ollama",
+                     description="Upload a PDF and enter a question to query the document.")
+iface.launch(share=True)
